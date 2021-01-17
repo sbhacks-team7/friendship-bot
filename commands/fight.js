fdb6d21831b66f27a6a3d587f6032dfe7c6daa3a
Alan Zhang <alan.zhang.052@gmail.com>
2021-01-16 16:23:23 -0800

    makeover

--- commands/fight.js
+++ commands/fight.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 
 module.exports= {
     name: 'fight',
-    description: "Description: Fights a mentioned user and generates a random winner",
-    usage: "Usage: $fight [@mention]\n\n",
+    description: "Fights a mentioned user and generates a random winner",
+    usage: "$fight [@mention]\n\n",
      execute(message, args){
         if (!message.mentions.users.size) {
             return message.reply('you need to tag a user in order to fight them!');
-        }        
+        }
         const taggedUser = message.mentions.users.first();
 
         if(Math.floor(Math.random() * 2)+1 === 1) {

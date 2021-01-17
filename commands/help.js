4e8cabd44ef84cd9cf85890148bfacb8db1b2aec
Bryan Xu <bryanbobo123@gmail.com>
2021-01-16 23:53:23 -0800

    bx - changed names in help.js

--- commands/help.js
+++ commands/help.js
@@ -5,28 +5,28 @@ module.exports= {
     name: 'help',
     description: "Sends a proper usage log to the chat",
     usage: "$help \t||\t $\n\n",
-     execute(message, args){
-       var list = [];
+    execute(message, args){
+        var list = [];
 
-       const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-       let result = commandFiles.forEach((file, i) => {
-           let props = require(`./${file}`);
-           var command = {
-               name: props.usage,
-               value: props.description,
-           };
+        const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+        let result = commandFiles.forEach((file, i) => {
+            let props = require(`./${file}`);
+            var command = {
+                name: props.usage,
+                value: props.description,
+            };
 
-           list.push(command);
-       });
+            list.push(command);
+        });
 
-       const embed = new Discord.MessageEmbed()
-           .setColor('#ff6666')
-           .setAuthor('Friendship Bot Help')
-           .setDescription('**Full Command List**')
-           .addFields(
-               list
-           )
-           .setFooter('Thanks for using Friendship Bot 💖')
-       message.channel.send(embed)
+        const embed = new Discord.MessageEmbed()
+            .setColor('#ff6666')
+            .setAuthor('Dachi Help')
+            .setDescription('**Full Command List**')
+            .addFields(
+                list
+            )
+            .setFooter('Thanks for using Dachi 💖')
+        message.channel.send(embed)
    }
 }

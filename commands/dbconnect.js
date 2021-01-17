4d2d13e9ac5a3049de05ed3a6cf58c2f3b96c164
Bryan Xu <bryanbobo123@gmail.com>
2021-01-17 00:21:55 -0800

    bx - fixed typo in help

--- commands/dbconnect.js
+++ commands/dbconnect.js
@@ -5,17 +5,17 @@ const client = new MongoClient(mongodb_uri, {useNewUrlParser: true, useUnifiedTo
 
 module.exports= {
     name: 'dbconnect',
-    description: "Function that connects to DB",
-    usage: "!dbconnect",
-     async execute(message, args){
-         try {
-             await client.connect();
-             console.log('Client connected to MongoDB!');
-             message.channel.send('Client connected to MongoDB!');
-         } catch(e) {
-             console.error(e);
-         } finally {
-             await client.close();
-         }
-    }
+    description: "Function that tests connection to mongoDB",
+    usage: "$dbconnect",
+    async execute(message, args){
+        try {
+            await client.connect();
+            console.log('Client connected to MongoDB!');
+            message.channel.send('Client connected to MongoDB!');
+        } catch(e) {
+            console.error(e);
+        } finally {
+            await client.close();
+        }
+}
 }

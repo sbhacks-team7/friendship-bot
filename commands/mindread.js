d9b6384ec8224374b088d34cdd322e0644729169
Bryan Xu <bryanbobo123@gmail.com>
2021-01-16 20:08:09 -0800

    bx - fixed mindread.js

--- commands/mindread.js
+++ commands/mindread.js
@@ -5,39 +5,49 @@ module.exports= {
     name: 'mindread',
     description: "Description: Sees if two users can read each others' minds!",
     usage: "Usage: $mindread [@mention]\n\n",
-     execute(message, args){
+    execute(message, args){
         if (!message.mentions.users.size) {
             return message.reply('you need to tag a user in order to read their mind!');
         }        
         const taggedUser = message.mentions.users.first();
-        let userReaction1, userReaction2;
+        if(taggedUser===null || taggedUser === undefined){
+            console.log("tagged user was null");
+        }
+
+        const challenge = new Discord.MessageEmbed().setDescription(`${message.author} has issued as mindreading challenge to <@` + `${taggedUser.id}` +">!");
+        message.channel.send(challenge);
+
         const embed = new Discord.MessageEmbed()
-            
             .setDescription("Do you accept this gift? 😊");
         
-        message.taggedUser.send(embed).then((question) => {
+        var taggedAnswer;
+        var userAnswer;
+
+        taggedUser.send(embed).then((question) => {
             question.react('👍');
             question.react('👎');
         
-
             const filter1 = (reaction, user) => {
                 return ['👍', '👎'].includes(reaction.emoji.name) && !user.bot && user.username==taggedUser.username;
             };
 
-
+        
             const collector1 = question.createReactionCollector(filter1, 
                 {max: 1, time: 60000
             });
 
             collector1.on('end', (collected, reason) => {
                 if (reason === 'time') {
-                  message.reply('Too late! Ran out of time...');
+                    message.reply('Too late! Ran out of time...');
+                    taggedAnswer = undefined;
                 } else {
-                  userReaction1 = collected.array()[0];
-                 
+                    console.log(reason);
+                    taggedAnswer = collected.array()[0];
+                    taggedAnswer = taggedAnswer._emoji.name;
                 }
             });
         });
+        
 
         message.author.send(embed).then((question) => {
             question.react('👍');
@@ -48,26 +58,43 @@ module.exports= {
             };
     
     
-            const collector2 = question.dmChannel.createReactionCollector(filter2, 
+            const collector2 = question.createReactionCollector(filter2, 
                 {max: 1, time: 60000
             });
 
             collector2.on('end', (collected, reason) => {
                 if (reason === 'time') {
-                  message.reply('Too late! Ran out of time...');
+                    message.reply('Too late! Ran out of time...');
+                    userAnswer = undefined;
                 } else {
-                  userReaction2 = collected.array()[0];
-                 
-                  if(userReaction1 === userReaction2) {
-                    message.channel.send("Your mental connection is strong!");
-                  } else {
-                    message.channel.send("Mindread failed...");
-                }
+                    console.log(reason);
+                    userAnswer = collected.array()[0];
+                    userAnswer = userAnswer._emoji.name;
                 }
             });
         });
 
-       
+
+        myVar = setTimeout(cont, 60000);
+        mySecondVar = setInterval(check, 3000);
+
+        function check(){
+            if(taggedAnswer !== undefined && userAnswer !== undefined){
+                clearTimeout(myVar);
+                clearInterval(mySecondVar);
+                cont();
+            }
+        }
+
+        function cont() {
+            console.log("Tagged asnwer: " + taggedAnswer);
+            console.log("\nUser answer: " + userAnswer + "\n");
+            if(taggedAnswer === undefined || userAnswer === undefined || taggedAnswer !== userAnswer) {
+                message.channel.send("Your mental connection is weak...");
+            } else {
+                message.channel.send("Your mental connection is strong!");
+            }
+        }
     
     }
 
